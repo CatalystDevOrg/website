3f9ceab1a09248206058dce16ef25c2fd154208f
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-28 11:53:34 +1300

    add basic front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,24 @@
-<h1>Hello, world!</h1>
+<h1 class="text-6xl leading-snug">
+	Surf the web with the <br />
+	<span class="text-emerald-400 font-bold">browser of the future.</span>
+</h1>
+<br />
+<br />
+<p class="text-xl">
+	This is <b class="text-emerald-400">Catalyst</b>. We're working to make a
+	fully-compatible with all web standards browser for the masses. We always
+	emphasise privacy and security, which is why we are
+	<b>fully open source</b>.
+</p>
+<br />
+<br />
+<hr />
+<a
+	href="https://github.com/JaydenDev/Catalyst/releases/latest"
+	class="hover:underline hover:text-gray-700 transition">Download Now</a
+>
+<p>
+	Made by
+  JaydenDev, webdev03, and other awesome
+	contributors.
+</p>
