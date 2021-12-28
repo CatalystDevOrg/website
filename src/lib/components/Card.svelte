7c38b9913355e1214e058d884195e7f37b5b8df7
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-28 19:52:24 +1300

    feat: add components for cards

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,3 @@
+<div class="inline-block p-3 w-72 h-96 rounded-md bg-gray-200 dark:bg-gray-800 dark:text-white text-gray-900">
+  <slot />
+</div>
---
+++ src/lib/components/CardTitle.svelte
@@ -0,0 +1,1 @@
+<h4 class="font-bold text-3xl"><slot /></h4>
