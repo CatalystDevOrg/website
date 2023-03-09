df1c84f6f513f4eea8dfdc41262b1a9e2312b1d3
Andrew B <bigspeedfpv@gmail.com>
2023-03-09 04:39:59 -0500

    feat: use astro assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  experimental: {
+    assets: true,
+  },
+  integrations: [tailwind()],
 });
