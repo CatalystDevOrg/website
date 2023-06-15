4644de72340934ca8dc286e1be7676ad3ad611ca
jdev08 <92550746+jdev082@users.noreply.github.com>
2023-06-14 19:42:55 -0500

    feat: debian/ubuntu repo instructions

--- src/components/InstallLinks.tsx
+++ src/components/InstallLinks.tsx
@@ -46,6 +46,17 @@ export default class InstallLinks extends React.Component<
           <br />
           scoop install ctlyst
         </div>
+        <br />
+        <span className="font-extralight">Debian/Ubuntu Linux</span>
+        <div className="font-mono font-normal bg-deepblue border-white/10 border-2 p-2 rounded-lg text-left text-sm shadow-md">
+           curl -s --compressed "https://catalystdevorg.github.io/Debian/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/Debian.gpg >/dev/null
+          <br />
+          sudo curl -s --compressed -o /etc/apt/sources.list.d/repository.list "https://catalystdevorg.github.io/Debian/repository.list
+          <br />
+          sudo apt update
+          <br />
+          sudo apt install catalyst
+        </div>
       </div>
     );
   }
